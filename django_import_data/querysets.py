3ea332f19f416b04508d81ffa4c510d828b621a9
Thomas Chamberlin <tchamber@nrao.edu>
2019-07-09 19:10:29 -0400

    Added annotations for successful/failed FI counts

--- django_import_data/querysets.py
+++ django_import_data/querysets.py
@@ -47,6 +47,13 @@ class FileImporterBatchQuerySet(DerivedValuesQueryset):
     def annotate_num_file_importers(self):
         return self.annotate(num_file_importers=Count("file_importers", distinct=True))
 
+    def annotate_num_successful_file_importers(self):
+        FileImporterBatch = apps.get_model("django_import_data.FileImporterBatch")
+        return self.annotate(num_successful_file_importers=Count("file_importers", distinct=True, filter=Q(status=FileImporterBatch.STATUSES.created_clean.db_value)))
+
+    def annotate_num_failed_file_importers(self):
+        FileImporterBatch = apps.get_model("django_import_data.FileImporterBatch")
+        return self.annotate(num_failed_file_importers=Count("file_importers", distinct=True, filter=Q(status__in=[status.db_value for status in FileImporterBatch.STATUSES if status != FileImporterBatch.STATUSES.created_clean])))
 
 class FileImporterQuerySet(DerivedValuesQueryset):
     @transaction.atomic
