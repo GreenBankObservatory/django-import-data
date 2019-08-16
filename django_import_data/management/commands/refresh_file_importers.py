91f078aa19ab9f3dc4ea34a19a1a146ee32f00b0
Thomas Chamberlin <tchamber@nrao.edu>
2019-08-16 14:26:44 -0400

    refresh_file_importers now only prints if it has things to refresh

--- django_import_data/management/commands/refresh_file_importers.py
+++ django_import_data/management/commands/refresh_file_importers.py
@@ -16,12 +16,13 @@ class Command(BaseCommand):
         since = kwargs.get("since", None)
         if since:
             file_importers = FileImporter.objects.filter(file_modified_on__gte=since)
-            since_str = f" (since {str(since)})"
+            since_str = f" (i.e. those modified since {str(since)})"
         else:
             file_importers = FileImporter.objects.all()
             since_str = ""
 
-        print(
-            f"Refreshing {file_importers.count()} File Importers from the filesystem{since_str}"
-        )
-        file_importers.refresh_from_filesystem()
+        if file_importers.count():
+            print(
+                f"Refreshing {file_importers.count()} File Importers from the filesystem{since_str}"
+            )
+            file_importers.refresh_from_filesystem()
