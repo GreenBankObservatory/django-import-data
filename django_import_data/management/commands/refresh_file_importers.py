5bdff0c16b99f20322e138a0b77d5186457a7fab
Thomas Chamberlin <tchamber@nrao.edu>
2019-08-16 11:18:31 -0400

    Can now refresh only file importers modified since a given time

--- django_import_data/management/commands/refresh_file_importers.py
+++ django_import_data/management/commands/refresh_file_importers.py
@@ -1,11 +1,27 @@
 from django.core.management.base import BaseCommand
 
 from django_import_data.models import FileImporter
+from django.utils.timezone import datetime
+
+
+def parse_date(date_str):
+    return datetime.strptime(date_str, r"%m/%d/%Y %H:%M:%S%z")
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("--since", type=parse_date)
+
     def handle(self, *args, **kwargs):
+        since = kwargs.get("since", None)
+        if since:
+            file_importers = FileImporter.objects.filter(file_modified_on__gte=since)
+            since_str = f" (since {str(since)})"
+        else:
+            file_importers = FileImporter.objects.all()
+            since_str = ""
+
         print(
-            f"Refreshing {FileImporter.objects.count()} File Importers from the filesystem"
+            f"Refreshing {file_importers.count()} File Importers from the filesystem{since_str}"
         )
-        FileImporter.objects.all().refresh_from_filesystem()
+        file_importers.refresh_from_filesystem()
