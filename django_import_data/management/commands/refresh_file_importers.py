d54308aa1598cc9dac58c6bc5e0d261e13661cfa
Thomas Chamberlin <tchamber@nrao.edu>
2019-08-20 16:05:24 -0400

    Bug fixes to refresh_file_importers

--- django_import_data/management/commands/refresh_file_importers.py
+++ django_import_data/management/commands/refresh_file_importers.py
@@ -16,60 +16,63 @@ def parse_date(date_str):
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("--paths", nargs="+")
-        parser.add_argument("--since", type=parse_date)
         parser.add_argument("--no-progress", action="store_true")
         parser.add_argument("--always-hash", action="store_true")
+        parser.add_argument("--quiet", action="store_true")
+        parser.add_argument("--all", action="store_true")
 
     @transaction.atomic
     def handle(self, *args, **kwargs):
-        since = kwargs.get("since", None)
         paths = kwargs.get("paths", None)
         quiet = kwargs.get("quiet", False)
+        refresh_all = kwargs.get("all", False)
         no_progress = kwargs.get("no_progress", False)
-        file_importers = FileImporter.objects.all()
-        since_str = ""
 
-        paths = paths if paths else []
-        if not isatty(sys.stdin.fileno()):
-            paths += [line.strip() for line in sys.stdin]
+        file_importers = FileImporter.objects.none()
+        if refresh_all:
+            file_importers = FileImporter.objects.all()
+        else:
+            paths = paths if paths else []
+            if not isatty(sys.stdin.fileno()):
+                paths += [line.strip() for line in sys.stdin]
 
-        if since:
-            file_importers |= FileImporter.objects.filter(file_modified_on__gte=since)
-            since_str = f" (i.e. those modified since {str(since)})"
+            if paths:
+                file_importers = FileImporter.objects.filter(file_path__in=paths)
 
-        if paths:
-            file_importers &= FileImporter.objects.filter(file_path__in=paths)
-
-        if not quiet:
-            print(
-                "The following recently-modified files do not have corresponding File Importers:"
-            )
-            print(
-                "\n  "
-                + "\n  ".join(
-                    sorted(
+                if not quiet:
+                    paths_not_yet_imported = sorted(
                         set(paths).difference(
                             set(file_importers.values_list("file_path", flat=True))
                         )
                     )
-                )
-            )
-            print("\nThe following recently-modified files will be refreshed:")
-            print(
-                "\n  "
-                + "\n  ".join(
-                    sorted(
-                        set(paths).intersection(
-                            set(file_importers.values_list("file_path", flat=True))
+
+                    if paths_not_yet_imported:
+                        print(
+                            "The following recently-modified files do not have corresponding "
+                            "File Importers:"
                         )
+                        print("  " + "\n  ".join(paths_not_yet_imported))
+
+                paths_to_be_refreshed = sorted(
+                    set(paths).intersection(
+                        set(file_importers.values_list("file_path", flat=True))
                     )
                 )
-            )
+
+                if paths_to_be_refreshed:
+                    print("\nThe following recently-modified files will be refreshed:")
+                    print("  " + "\n  ".join(paths_to_be_refreshed))
+            else:
+                if not quiet:
+                    print("No paths given!")
+
+        if not quiet:
+            print("\n")
+        if file_importers.count():
             print(
                 f"Refreshing {file_importers.count()} File Importers "
-                f"from the filesystem{since_str}"
+                f"from the filesystem"
             )
-        if file_importers.count():
             report = file_importers.refresh_from_filesystem(
                 quiet=no_progress, always_hash=kwargs["always_hash"]
             )
@@ -78,3 +81,5 @@ class Command(BaseCommand):
                 if status in ["changed"]:
                     the_paths = "\n".join([fi.file_path for fi in file_importers])
                     print(f"{status}\n{'-' * len(status)}\n{the_paths}\n")
+        elif not quiet:
+            print("No file importers to refresh!")
